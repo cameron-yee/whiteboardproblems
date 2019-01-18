0a7eda9248e036a803ea77982c0574499b1d7ad1
cameron-yee <yee.camero23@gmail.com>
2019-01-18 09:17:06 -0700

    Added solution for Anagrams

---
+++ anagrams.js
@@ -0,0 +1,18 @@
+const isAnagram = (input_one, input_two) => {
+    input_one_characters = input_one.split('').sort()
+    input_two_characters = input_two.split('').sort()
+
+    if(input_one_characters.length === input_two_characters.length) {
+        for(let i = 0; i < input_one_characters.length; i++) {
+            if(input_one_characters[i].toString() !== input_two_characters[i].toString()) {
+               return false 
+            }
+        }
+        return true
+    } else {
+        return false
+    }
+}
+
+const solution = isAnagram(process.argv[2], process.argv[3])
+console.log(solution)
